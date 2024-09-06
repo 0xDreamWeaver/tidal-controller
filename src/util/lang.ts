f622a41afa5bdd67f95ffe98fa60f305814b759b
Dreamweaver <0xdreamweaver@tutanota.com>
2024-09-05 18:48:59 -0700

    Correct all translations Prepare at add "volume up" and "volume down" fn

--- src/util/lang.ts
+++ src/util/lang.ts
@@ -27,6 +27,8 @@ export type MenuOptions = {
   playPrevious: string;
   playRepeat: string;
   playRepeatOne: string;
+  volUp: string;
+  volDown: string;
 };
 
 export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
@@ -34,41 +36,47 @@ export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
     case "Bulgarian":
       return {
         playback: "Възпроизвеждане",
-        shuffle: "Случайна",
-        next: "Следваща",
-        previous: "Предишна",
-        play: "Пусни",
-        pause: "Пауза",
+        shuffle: "Разбъркай",
+        next: "Следващ/а",
+        previous: "Предишен/а",
+        play: "Пауза",
+        pause: "Пусни",
         playNext: "Пусни следващата песен",
         playPrevious: "Пусни предишната песен",
-        playRepeat: "Повтаряне",
+        playRepeat: "Повторение",
         playRepeatOne: "Повтаряй една песен",
+        volUp: "Увеличаване на силата на звука",
+        volDown: "Намаляване на силата на звука",
       };
     case "Danish":
       return {
-        playback: "Afspilning",
-        shuffle: "Tilfældig",
+        playback: "Afspiller",
+        shuffle: "Shuffle",
         next: "Næste",
         previous: "Forrige",
-        play: "Spil",
-        pause: "Pause",
+        play: "Pause",
+        pause: "Play",
         playNext: "Spil næste sang",
         playPrevious: "Spil forrige sang",
         playRepeat: "Gentag",
         playRepeatOne: "Gentag en sang",
+        volUp: "Lydstyrke op",
+        volDown: "Lydstyrke ned",
       };
     case "German":
       return {
         playback: "Wiedergabe",
-        shuffle: "Zufallswiedergabe",
+        shuffle: "Shuffle",
         next: "Weiter",
         previous: "Zurück",
-        play: "Abspielen",
-        pause: "Pause",
+        play: "Pause",
+        pause: "Abspielen",
         playNext: "Nächsten Titel abspielen",
         playPrevious: "Vorherigen Titel abspielen",
         playRepeat: "Wiederholen",
         playRepeatOne: "Einen Titel wiederholen",
+        volUp: "Lauter",
+        volDown: "Leiser",
       };
     case "English":
       return {
@@ -76,12 +84,14 @@ export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
         shuffle: "Shuffle",
         next: "Next",
         previous: "Previous",
-        play: "Play",
-        pause: "Pause",
+        play: "Pause",
+        pause: "Play",
         playNext: "Play Next Track",
         playPrevious: "Play Previous Track",
         playRepeat: "Repeat",
         playRepeatOne: "Repeat One",
+        volUp: "Volume up",
+        volDown: "Volume down",
       };
     case "Spanish":
       return {
@@ -89,12 +99,14 @@ export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
         shuffle: "Aleatorio",
         next: "Siguiente",
         previous: "Anterior",
-        play: "Reproducir",
-        pause: "Pausa",
+        play: "Pausa",
+        pause: "Reproducir",
         playNext: "Reproducir siguiente pista",
         playPrevious: "Reproducir pista anterior",
         playRepeat: "Repetir",
         playRepeatOne: "Repetir una",
+        volUp: "Subir el volumen",
+        volDown: "Bajar el volumen",
       };
     case "French":
       return {
@@ -102,51 +114,59 @@ export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
         shuffle: "Aléatoire",
         next: "Suivant",
         previous: "Précédent",
-        play: "Lecture",
-        pause: "Pause",
+        play: "Pause",
+        pause: "Jouer",
         playNext: "Lire le morceau suivant",
         playPrevious: "Lire le morceau précédent",
         playRepeat: "Répéter",
         playRepeatOne: "Répéter un morceau",
+        volUp: "Augmenter le volume",
+        volDown: "Diminuer le volume",
       };
     case "Croatian":
       return {
         playback: "Reprodukcija",
         shuffle: "Nasumično",
-        next: "Sljedeće",
+        next: "Dalje",
         previous: "Prethodno",
-        play: "Reproduciraj",
-        pause: "Pauza",
+        play: "Pauza",
+        pause: "Reproduciraj",
         playNext: "Reproduciraj sljedeću pjesmu",
         playPrevious: "Reproduciraj prethodnu pjesmu",
         playRepeat: "Ponovi",
         playRepeatOne: "Ponovi jednu pjesmu",
+        volUp: "Veća glasnoća",
+        volDown: "Manja glasnoća",
       };
     case "Italian":
       return {
         playback: "Riproduzione",
-        shuffle: "Riproduzione casuale",
-        next: "Avanti",
-        previous: "Indietro",
-        play: "Riproduci",
-        pause: "Pausa",
+        shuffle: "Shuffle",
+        next: "Successivo",
+        previous: "Precedente",
+        play: "Pausa",
+        pause: "Play",
         playNext: "Riproduci brano successivo",
         playPrevious: "Riproduci brano precedente",
         playRepeat: "Ripeti",
         playRepeatOne: "Ripeti un brano",
+        volUp: "Alza volume",
+        volDown: "Abbassa volume",
       };
     case "Norwegian":
       return {
         playback: "Avspilling",
-        shuffle: "Tilfeldig rekkefølge",
+        shuffle: "Shuffle",
         next: "Neste",
         previous: "Forrige",
-        play: "Spill av",
-        pause: "Pause",
+        play: "Pause",
+        pause: "Spill av",
         playNext: "Spill neste spor",
         playPrevious: "Spill forrige spor",
-        playRepeat: "Gjenta",
+        playRepeat: "Repeter",
         playRepeatOne: "Gjenta én",
+        volUp: "Lydstyrke opp",
+        volDown: "Lydstyrke ned",
       };
     case "Polish":
       return {
@@ -154,64 +174,74 @@ export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
         shuffle: "Losowo",
         next: "Następny",
         previous: "Poprzedni",
-        play: "Odtwórz",
-        pause: "Wstrzymaj",
+        play: "Pauza",
+        pause: "Odtwórz",
         playNext: "Odtwórz następny utwór",
         playPrevious: "Odtwórz poprzedni utwór",
-        playRepeat: "Powtórz",
+        playRepeat: "Powtarzaj",
         playRepeatOne: "Powtórz jeden",
+        volUp: "Głośniej",
+        volDown: "Ciszej",
       };
     case "PortugueseBrazil":
       return {
         playback: "Reprodução",
         shuffle: "Aleatório",
-        next: "Próxima",
-        previous: "Anterior",
-        play: "Reproduzir",
-        pause: "Pausar",
+        next: "Próximo",
+        previous: "Voltar",
+        play: "Pausa",
+        pause: "Tocar",
         playNext: "Reproduzir próxima faixa",
         playPrevious: "Reproduzir faixa anterior",
         playRepeat: "Repetir",
         playRepeatOne: "Repetir uma",
+        volUp: "Aumentar Volume",
+        volDown: "Diminuir Volume",
       };
     case "Portuguese":
       return {
         playback: "Reprodução",
         shuffle: "Aleatório",
-        next: "Seguinte",
+        next: "Próximo",
         previous: "Anterior",
-        play: "Reproduzir",
-        pause: "Pausa",
+        play: "Pausa",
+        pause: "Tocar",
         playNext: "Reproduzir faixa seguinte",
         playPrevious: "Reproduzir faixa anterior",
         playRepeat: "Repetir",
         playRepeatOne: "Repetir uma",
+        volUp: "Aumentar volume",
+        volDown: "Diminuir volume",
       };
     case "Slovenian":
       return {
         playback: "Predvajanje",
-        shuffle: "Naključno",
-        next: "Naprej",
-        previous: "Nazaj",
-        play: "Predvajaj",
-        pause: "Premor",
+        shuffle: "Naključno prerazporedi",
+        next: "Naslednje",
+        previous: "Prejšnje",
+        play: "Premor",
+        pause: "Predvajaj",
         playNext: "Predvajaj naslednjo skladbo",
         playPrevious: "Predvajaj prejšnjo skladbo",
         playRepeat: "Ponovi",
         playRepeatOne: "Ponovi eno",
+        volUp: "Povišaj glasnost",
+        volDown: "Znižaj glasnost",
       };
     case "Serbian":
       return {
         playback: "Reprodukcija",
         shuffle: "Nasumično",
-        next: "Sledeća",
-        previous: "Prethodna",
-        play: "Pusti",
-        pause: "Pauza",
+        next: "Sledeće",
+        previous: "Prethodno",
+        play: "Pauziraj",
+        pause: "Reprodukuj",
         playNext: "Pusti sledeću pesmu",
         playPrevious: "Pusti prethodnu pesmu",
         playRepeat: "Ponovi",
         playRepeatOne: "Ponovi jednu",
+        volUp: "Povećaj jačinu zvuka",
+        volDown: "Smanji jačinu zvuka",
       };
     case "Swedish":
       return {
@@ -219,25 +249,29 @@ export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
         shuffle: "Blanda",
         next: "Nästa",
         previous: "Föregående",
-        play: "Spela",
-        pause: "Pausa",
+        play: "Pausa",
+        pause: "Spela",
         playNext: "Spela nästa spår",
         playPrevious: "Spela föregående spår",
-        playRepeat: "Upprepa",
+        playRepeat: "Repetera",
         playRepeatOne: "Upprepa en",
+        volUp: "Öka volymen",
+        volDown: "Sänk volymen",
       };
     case "Turkish":
       return {
         playback: "Oynatma",
         shuffle: "Karıştır",
-        next: "Sonraki",
+        next: "İleri",
         previous: "Önceki",
-        play: "Çal",
-        pause: "Duraklat",
+        play: "Duraklat",
+        pause: "Yürüt",
         playNext: "Sonraki parçayı çal",
         playPrevious: "Önceki parçayı çal",
         playRepeat: "Tekrarla",
         playRepeatOne: "Bir kez tekrarla",
+        volUp: "Sesi aç",
+        volDown: "Sesi kıs",
       };
     default:
       return {
@@ -245,12 +279,14 @@ export const getMenuOptionsByLanguage = (code: LanguageCode): MenuOptions => {
         shuffle: "Shuffle",
         next: "Next",
         previous: "Previous",
-        play: "Play",
-        pause: "Pause",
+        play: "Pause",
+        pause: "Play",
         playNext: "Play Next Track",
         playPrevious: "Play Previous Track",
         playRepeat: "Repeat",
         playRepeatOne: "Repeat One",
+        volUp: "Volume up",
+        volDown: "Volume down",
       };
   }
 };
